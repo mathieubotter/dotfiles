b51142d3b06dc323c4ce8fb8fdae171ad3b1c1af
Mathieu Botter <mathieubotter@outlook.com>
2016-09-26 00:01:34 +0200

    Update install script for macOS

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Homebrew
 echo "Installing Homebrew..."
--- macos/install.sh
+++ macos/install.sh
@@ -1,2 +1,5 @@
+echo "Set macOS defaults"
+exec ./set-defaults.sh
+
 echo "Update Mac App"
 sudo softwareupdate -i -a
