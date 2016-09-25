cf08eccbec4ba16b0cfb835c3a9285510af17185
Mathieu Botter <mathieubotter@outlook.com>
2016-09-26 00:05:01 +0200

    Add zsh prompt config

---
+++ zsh/prompt.zsh
@@ -0,0 +1,13 @@
+# Get the Git branch
+parse_git_branch() {
+  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+}
+
+# Custom bash prompt
+#
+# Includes custom character for the prompt, path, and Git branch name.
+#
+# Source: kirsle.net/wizards/ps1.html
+export PS1="\n\[$(tput bold)\]\[$(tput setaf 5)\]➜ \[$(tput setaf 6)\]\w\[$(tput setaf 3)\]\$(parse_git_branch) \[$(tput sgr0)\]"
+
+export PATH=/opt/local/bin:/opt/local/sbin:${PATH}
